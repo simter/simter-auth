768eee3a3455db9f219b6720a63604ba849ef18b
RJ <rongjihuang@gmail.com>
2019-02-14 18:01:16 +0800

    Create postgres SQL

---
+++ simter-auth-data/src/main/resources/tech/simter/auth/sql/postgres/data.sql
@@ -0,0 +1,10 @@
+﻿/**
+ * Data initialize script.
+ * @author RJ
+ */
+
+-- admin
+insert into st_user(id, status, code, name, create_on)
+  values (1, 1, '001', 'Admin', date_trunc('second', current_timestamp));
+insert into st_account(id, status, username, password, create_on)
+  values (1, 1, 'admin', '21218cca77804d2ba1922c33e0151105', date_trunc('second', current_timestamp)); -- 888888
