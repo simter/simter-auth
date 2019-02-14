9d3aef0a22cda531aa40c5312ded0c47693a1d4e
RJ <rongjihuang@gmail.com>
2019-02-14 18:01:06 +0800

    Create h2 SQL

---
+++ simter-auth-data/src/main/resources/tech/simter/auth/sql/h2/schema-drop.sql
@@ -0,0 +1,10 @@
+/**
+ * Drop table script.
+ * @author RJ
+ */
+
+drop table if exists st_relation;
+drop table if exists st_account;
+drop table if exists st_user;
+drop table if exists st_role;
+drop table if exists st_organization;
