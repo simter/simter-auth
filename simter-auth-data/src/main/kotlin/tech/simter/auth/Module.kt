129321ef9b0b2f0ac579bc10c2f03ad996e3f5f8
RJ <rongjihuang@gmail.com>
2019-02-14 17:52:39 +0800

    Seperate st_actor table to st_organization, st_user, st_role

--- simter-auth-data/src/main/kotlin/tech/simter/auth/Module.kt
+++ simter-auth-data/src/main/kotlin/tech/simter/auth/Module.kt
@@ -4,11 +4,15 @@ package tech.simter.auth
 const val PACKAGE_NAME = "tech.simter.auth"
 
 // all table name
+
 const val ACCOUNT_TABLE = "st_account"
-const val ACTOR_TABLE = "st_actor"
-const val COMPANY_TABLE = ACTOR_TABLE
-const val DEPARTMENT_TABLE = ACTOR_TABLE
-const val TEAM_TABLE = ACTOR_TABLE
+
+const val ORGANIZATION_TABLE = "st_organization"
+const val COMPANY_TABLE = ORGANIZATION_TABLE
+const val DEPARTMENT_TABLE = ORGANIZATION_TABLE
+const val TEAM_TABLE = ORGANIZATION_TABLE
+
 const val USER_TABLE = "st_user"
+const val ROLE_TABLE = "st_role"
+
 const val RELATION_TABLE = "st_relation"
-const val ROLE_TABLE = ACTOR_TABLE
